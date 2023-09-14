6cc624b3cdf3165de50c215c5564a3665cc962a4
svrznjak <gregor.svrznjak@gmail.com>
2023-09-14 11:11:47 +0200

    fix vee validate locale set

--- src/global/utils/setLocale.ts
+++ src/global/utils/setLocale.ts
@@ -1,10 +1,31 @@
 import { useI18n } from 'vue-i18n'
+import getDefaultTranslationsList from '@/plugins/VeeValidateTranslations/defaultTranslationsList'
 import { setLocale as setVeeValidateLocale } from '@vee-validate/i18n'
 
 let i18n: any = undefined
 
-export default function setLocale(newLocale: string) {
+export default async function setLocale(newLocale: string) {
   if (!i18n) i18n = useI18n()
   i18n.locale.value = newLocale
-  setVeeValidateLocale(newLocale)
+
+  // set vee-validate locale
+  // get all available locales
+  const translations = await getDefaultTranslationsList()
+  const codes = translations.map((translation) => {
+    return translation.code.replace('_', '-')
+  })
+
+  // if newLocale is in codes, set it
+  if (codes.includes(newLocale)) {
+    setVeeValidateLocale(newLocale)
+  } else {
+    // if newLocale is not in codes, try to set it with the first two letters
+    const newLocaleShort = newLocale.substring(0, 2)
+    if (codes.includes(newLocaleShort)) {
+      setVeeValidateLocale(newLocaleShort)
+    } else {
+      // if newLocale is not in codes, set it to english
+      setVeeValidateLocale('en')
+    }
+  }
 }
--- src/plugins/VeeValidatePlugin.ts
+++ src/plugins/VeeValidatePlugin.ts
@@ -1,16 +1,13 @@
 // import all default locales from @vee-validate/i18n
-import defaultTranslationsList from './VeeValidateTranslations/defaultTranslationsList'
-
 import { Form, defineRule, configure } from 'vee-validate'
-import FieldInput from '@/global/components/FieldInput.vue'
-import FieldListInput from '@/global/components/FieldListInput.vue'
 import rules from '@vee-validate/rules'
 import { localize } from '@vee-validate/i18n'
 
 import defaultVeeTranslation from './VeeValidateTranslations/en-US.json'
 import locales from '@/global/data/locales.json'
-import { get } from 'http'
 import getDefaultTranslationsList from './VeeValidateTranslations/defaultTranslationsList'
+
+import VueI18n from 'vue-i18n'
 
 export default {
   install: (app: any) => {
@@ -18,13 +15,6 @@ export default {
     Object.keys(rules).forEach((rule) => {
       defineRule(rule, rules[rule])
     })
-
-    // Set en-US as translation for all languages to ensure default is working well
-    for (const locale in locales) {
-      configure({
-        generateMessage: localize(locale, { messages: defaultVeeTranslation.messages })
-      })
-    }
 
     //Import all translations from i18n package
     ;(async () => {
@@ -41,13 +31,12 @@ export default {
     //Import all translations from adjacent folder and overwrite translations
     ;(async () => {
       const imports = import.meta.glob('./VeeValidateTranslations/*.json')
-      console.log(imports)
       for (const path in imports) {
         const language: any = await imports[path]()
         const code = language.code
         const messages = { messages: language.messages }
         configure({
-          generateMessage: localize(code, messages)
+          generateMessage: localize(code.replace('_', '-'), messages)
         })
       }
     })()
